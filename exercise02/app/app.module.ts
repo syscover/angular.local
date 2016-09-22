8eb4adf25ecb860a8b0bbc02c8df6aecca956ec8
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-09-23 00:44:39 +0200

    exercise02

--- exercise02/app/app.module.ts
+++ exercise02/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
 
 import { AppComponent }   from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent }   from './app.component';
         FormsModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        HeroDetailComponent
     ],
     bootstrap: [
         AppComponent
