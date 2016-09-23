c3a6dd84a5506b950b9409ced1a225049b7f1bdd
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-09-23 21:33:08 +0200

    practicas

--- exercise02/app/app.component.ts
+++ exercise02/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+import { HeroService} from './hero.service';
 
 @Component({
     selector: 'my-app',
+    providers: [HeroService],
     template:`
         <h1>{{title}}</h1>
         
@@ -84,8 +73,19 @@ const HEROES: Hero[] = [
 
 export class AppComponent {
     title   = 'Tour of Heroes';
-    heroes  = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
+
+    constructor(private heroService: HeroService)
+    {
+        this.heroes = this.heroService.getHeroes();
+    }
+
+
+    // NOS HEMOS QUEDADO AQUI!!!
+    // getHeroes(): void {
+    //     this.heroes = this.heroService.getHeroes();
+    // }
 
     onSelect(hero:Hero):void
     {
