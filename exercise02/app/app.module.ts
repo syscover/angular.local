0bf0242f634e499c6336c754de6faa72a0f0fcd1
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-09-28 23:38:50 +0200

    routing

--- exercise02/app/app.module.ts
+++ exercise02/app/app.module.ts
@@ -1,22 +1,33 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
+
+import { routing } from './app.routing';
 
 import { AppComponent }   from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        routing
     ],
     declarations: [
         AppComponent,
-        HeroDetailComponent
+        HeroDetailComponent,
+        HeroesComponent
+    ],
+    providers: [
+        HeroService
     ],
     bootstrap: [
         AppComponent
     ]
 })
 
-export class AppModule { }
+export class AppModule {
+}
